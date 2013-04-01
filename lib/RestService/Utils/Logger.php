31c37ffb74d3da11ebd1f45bea32e251d3c565ab
François Kooman <fkooman@tuxed.net>
2013-04-01 19:12:04 +0200

    update copyright to 2013

--- lib/RestService/Utils/Logger.php
+++ lib/RestService/Utils/Logger.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* Copyright 2012 François Kooman <fkooman@tuxed.net>
+* Copyright 2013 François Kooman <fkooman@tuxed.net>
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
